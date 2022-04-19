f9f23cb7be3865234bd0a22040b61872b8e1213b
LokiLP66 <68025706+LokiLP66@users.noreply.github.com>
2022-04-19 18:42:39 +0200

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ import { Link } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Hugo Berendi. <br></br>
+      &copy; {new Date().getFullYear()} Hugo Berendi all rights reserved. <br></br>
       inspired by {' '}<NextLink href="https://www.craftz.dog/"><Link href="https://www.craftz.dog/" target="_blank" >craftz.dog</Link></NextLink>
     </Box>
   )

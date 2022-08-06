89ab37e3519eef782b1e7f6fb42cee1c490f84a1
LokiLP66 <68025706+LokiLP66@users.noreply.github.com>
2022-08-06 23:49:07 +0200

    .

--- pages/works/fumiko.js
+++ pages/works/fumiko.js
@@ -6,7 +6,7 @@ import Layout from '../../components/layouts/article'
 const Work = () => (
     <Layout title="Fumiko">
         <Container>
-            <iframe src="http://2.59.156.228/" width="100vw" height="100vh"></iframe>
+            <iframe src="http://2.59.156.228/" width="100%" height="100%"></iframe>
         </Container>
     </Layout>
 )

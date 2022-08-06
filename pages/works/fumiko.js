ae7798c305c8daf41f178da72162a0c718232b32
LokiLP66 <68025706+LokiLP66@users.noreply.github.com>
2022-08-06 23:38:00 +0200

    .

--- pages/works/fumiko.js
+++ pages/works/fumiko.js
@@ -6,44 +6,18 @@ import {
     Heading,
     Center,
     Image
-  } from '@chakra-ui/react'
-  import Layout from '../../components/layouts/article'
-  import { Title, Meta } from '../../components/work'
-  import P from '../../components/paragraph'
-  
-  const Work = () => (
+} from '@chakra-ui/react'
+import Layout from '../../components/layouts/article'
+import { Title, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+
+const Work = () => (
     <Layout title="Fumiko">
-      <Container>
-        <head>
-            <meta httpEquiv='refresh' content='0; URL=http://2.59.156.228/' />
-        </head>
-        <Title>
-          Fumiko <Badge>2022-????</Badge>
-        </Title>
-        <Center my={6}>
-          <Image src="/images/" alt="icon" />
-        </Center>
-        <P>
-          Fumiko is a all in one discord bot!
-        </P>
-  
-        <List ml={4} my={4}>
-          <ListItem>
-            <Meta>Stack</Meta>
-            <span>Typescript, Node.js, Discord.js</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Last update</Meta>
-            <span>None</span>
-          </ListItem>
-        </List>
-  
-        <Heading as="h4" fontSize={16} my={6}>
-          <Center>Media coverage</Center>
-        </Heading>
-      </Container>
+        <Container>
+            <iframe src="http://2.59.156.228/"></iframe>
+        </Container>
     </Layout>
-  )
-  
-  export default Work
-  export { getServerSideProps } from '../../components/chakra'
+)
+
+export default Work
+export { getServerSideProps } from '../../components/chakra'
